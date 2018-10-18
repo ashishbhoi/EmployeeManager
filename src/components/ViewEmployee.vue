015c7dcc563881583f75d46a27142f8a975cb94c
Ashish Kumar Bhoi <ashish.bhoi12@gmail.com>
2018-10-18 21:57:50 +0530

    Now you can view And delete  employee data

--- src/components/ViewEmployee.vue
+++ src/components/ViewEmployee.vue
@@ -1,15 +1,58 @@
 <template>
     <div id="view-employee">
-        <h3>View Employee</h3>
+        <ul class="collection with-header">
+            <li class="collection-header"><h4>ID : {{employee_id}}</h4></li>
+            <li class="collection-item">Name : {{name}}</li>
+            <li class="collection-item">Department : {{dept}}</li>
+            <li class="collection-item">Position : {{position}}</li>
+        </ul>
+        <router-link to="/" class="btn grey">Back</router-link>
+        <button @click="deleteEmployee" class="btn red">Delete</button>
     </div>
 </template>
 
 <script>
+import db from './firebaseInit'
 export default {
     name: 'view-employee',
     data () {
         return {
-            
+            employee_id: null,
+            name: null,
+            dept: null,
+            position: null
+        }
+    },
+    beforeRouteEnter(to, from, next) {
+        db.collection('employees').doc( to.params.employee_id).get()
+        .then(function(doc) {
+                next(vm => {
+                    vm.employee_id = doc.data().employee_id
+                    vm.name = doc.data().name
+                    vm.dept = doc.data().dept
+                    vm.position = doc.data().position
+                })
+        })
+    },
+    watch: {
+        '$route': 'fetchData'
+    },
+    methods: {
+        fetchData() {
+            db.collection('employees').doc(this.$root.params.employee_id).get().then(function(doc) {
+                this.employee_id = doc.data().employee_id
+                this.name = doc.data().name
+                this.dept = doc.data().dept
+                this.position = doc.data().position   
+            })
+        },
+        deleteEmployee() {
+            if(confirm('Are You Sure?')){
+            db.collection('employees').doc(this.$root.params.employee_id).get().then(function(doc) {
+                doc.ref.delete()
+                this.$router('/')
+            })
+            }
         }
     }
 }
