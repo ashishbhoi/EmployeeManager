41d043473602c7b6bf9a1ed9adfffd35ae391cf5
Ashish Kumar Bhoi <ashish.bhoi12@gmail.com>
2018-10-18 19:52:33 +0530

    Routs Added Materalize Css Assed Fontawesome Added

---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+    <nav>
+        <div class="nav-wrapper green">
+            <div class="container ">
+                <div class="left-align">
+                <router-link to="/" class="brand-logo">Employee Manager</router-link>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
